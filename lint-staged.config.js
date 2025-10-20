068027292f1b7cb2ceb7b11f150af0122aaf4639
Matt Franceschini <franceschinimatt@gmail.com>
2025-10-19 20:00:51 -0400

    update lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "src/**/*.{js,jsx,ts,tsx}": [`prettier --write`, `eslint --fix`],
+  "client/**/*.{js,jsx,ts,tsx}": [`prettier --write`, `eslint --fix`],
+  "server/**/*.{js,jsx,ts,tsx}": [`prettier --write`, `eslint --fix`],
+  "shared/**/*.{js,jsx,ts,tsx}": [`prettier --write`, `eslint --fix`],
 }
